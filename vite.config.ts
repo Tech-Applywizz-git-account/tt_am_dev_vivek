5e272fbe9904fd701854f596134f538c4fd41b0f
Tech-Applywizz-git-account <techteam@Applywizz.com>
2025-11-03 15:28:29 +0530

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // ✅ required for @ to work
