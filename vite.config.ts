b70b90a3ba24629fbbb737bcaf190c4477016ccb
Tech-Applywizz-git-account <techteam@Applywizz.com>
2025-11-13 12:17:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   // server: { port:3000 },
+  
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // ✅ required for @ to work
