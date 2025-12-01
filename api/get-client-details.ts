27c872f65f98796d76d2b8927aaf9fdbda09db75
Tech-Applywizz-git-account <techteam@Applywizz.com>
2025-12-01 15:26:10 +0530

    Enable CORS in get-client-details API
    Added CORS headers to enable cross-origin requests.

--- api/get-client-details.ts
+++ api/get-client-details.ts
@@ -17,6 +17,7 @@ const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, {
 });
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+    
     // Enable CORS for cross-origin requests
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
