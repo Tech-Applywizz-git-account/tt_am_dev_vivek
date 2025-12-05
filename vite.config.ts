5f693ef888bb6ec43b162b732c6701121d7ce68f
Tech-Applywizz-git-account <techteam@Applywizz.com>
2025-12-05 09:43:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+
 export default defineConfig({
   plugins: [react()],
   // server: { port:3000 },
