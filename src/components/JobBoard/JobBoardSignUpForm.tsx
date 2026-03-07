dcc3de1134370226c8dd5431a792d7f90ba2549c
Tech-Applywizz-git-account <techteam@Applywizz.com>
2026-03-07 09:28:21 +0530

    Revert "Devlopment"

--- src/components/JobBoard/JobBoardSignUpForm.tsx
+++ src/components/JobBoard/JobBoardSignUpForm.tsx
@@ -275,6 +275,7 @@ const JobBoardSignUpForm: React.FC<JobBoardSignUpFormProps> = ({ onSignUpSuccess
                 .from('clients')
                 .select('id')
                 .ilike('company_email', userEmail)
+                .eq('opted_job_links', true)
                 .maybeSingle();
 
             if (existingClientWithJob) {
